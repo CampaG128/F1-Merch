b0d04ad21cb685ad303e6bf9cf64496f58424146
CampaG128 <143743923+CampaG128@users.noreply.github.com>
2023-09-25 12:43:24 -0600

    Delete Pits directory

--- Pits/js/index.js
+++
@@ -1,6 +0,0 @@
-const formulario = document.getElementById('formularios');
-const btn = document.getElementById('btn-formularios');
-
-btn.addEventListener('click', ()=> {
-    formulario.classList.remove('ocultar');
-});
